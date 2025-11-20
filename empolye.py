3b86aba6f20027ee13841611737d875180071a4f
sanjanapadashetti <312sanjanap@gmail.com>
2025-11-20 15:44:29 +0530

    Update empolye.py

--- empolye.py
+++ empolye.py
@@ -1,7 +1,7 @@
 import sys
    #check if correct number of argument a
 if len(sys.argv) != 5:
-   print("Usage: python empoloye.py <name> <userid> <salary> <expirence>") 
+   print("Usage: python empoloye.py <name> <userid> <salary> <experience>") 
    sys.exit(1)
 
 # sys.argv[0] is always the program name
@@ -9,10 +9,10 @@ script_name = sys.argv[0]
 name = sys.argv[1]
 userid = sys.argv[2]
 salary = sys.argv[3]
-expirence = sys.argv[4]
+experience = sys.argv[4]
 
 print("Script name:", script_name)
 print("employe name:", name)
 print("empolye id:",id)
 print("employe salary:",salary)
-print("employe expirence:",expirence)
+print("employe expirence:",experience)
