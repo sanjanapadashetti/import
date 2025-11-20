907a7c90b78c6120478764d9972fa4161e6b6e14
sanjanapadashetti <312sanjanap@gmail.com>
2025-11-20 14:33:24 +0530

    Create import.py

---
+++ import.py
@@ -0,0 +1,13 @@
+import sys
+#check if correct number of argument a
+if len(sys.argv) != 3:
+   print("Usage: python student.py <name> <roll sys.exit(1)
+
+# sys.argv[0] is always the program name
+script_name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]
+
+print("Script name:", script_name)
+print("Student name:", name)
+print("Roll number:",rollno)
